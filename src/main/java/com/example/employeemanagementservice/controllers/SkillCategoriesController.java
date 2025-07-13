7694606ed4270b24a2453af07e999ca24023afc0
Emil Kerimov <emilkerimovofficial@gmail.com>
2025-07-13 23:54:51 +0300

    add tag in SkillCategoriesController

--- src/main/java/com/example/employeemanagementservice/controllers/SkillCategoriesController.java
+++ src/main/java/com/example/employeemanagementservice/controllers/SkillCategoriesController.java
@@ -7,6 +7,7 @@ import com.example.employeemanagementservice.services.SkillCategoriesService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -15,6 +16,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/skill-categories")
+@Tag(name = "Skill Categories management", description = "API for viewing, adding, deleting, and updating Skill Categories")
 public class SkillCategoriesController {
     private final SkillCategoriesService skillCategoriesService;
 
